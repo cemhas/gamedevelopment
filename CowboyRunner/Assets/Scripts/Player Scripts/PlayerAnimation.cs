a02207a30b9b5fd9025e10e5d214b80980106e50
cemhas <haspolatcem@gmail.com>
2021-04-11 18:41:29 +0300

    Cowboy runner game added

---
+++ CowboyRunner/Assets/Scripts/Player Scripts/PlayerAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if(target.gameObject.tag == "Obstacle")
+        {
+            anim.Play("Idle");
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D target)
+    {
+        if (target.gameObject.tag == "Obstacle")
+        {
+            anim.Play("Run");
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CowboyRunner/Assets/Scripts/Player Scripts/PlayerDied.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDied : MonoBehaviour
+{
+
+    public delegate void EndGame();
+    public static event EndGame endGame;
+
+    void PlayerDiedEndGame()
+    {
+        if (endGame != null)
+            endGame();
+
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Colector")
+        {
+            PlayerDiedEndGame();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D target)
+    {
+        if (target.gameObject.tag == "Zombie")
+        {
+            PlayerDiedEndGame();
+        }
+    }
+
+}
